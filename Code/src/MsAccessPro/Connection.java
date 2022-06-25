90946dd6815e49c822c543deaf7849fe467d3ab7
akshatmardia <54734873+akshatmardia@users.noreply.github.com>
2022-06-25 16:13:13 -0700

    Add files via upload

---
+++ Code/src/MsAccessPro/Connection.java
@@ -0,0 +1,28 @@
+package src.MsAccessPro;
+
+import java.sql.DriverManager;
+
+public class Connection {
+    public static java.sql.Connection con;
+    public static String dbFile = "D:\\Athletics.accdb";
+    public static String dbUrl;
+
+    public Connection() {
+    }
+
+    public static java.sql.Connection getConnection() {
+        try {
+            con = DriverManager.getConnection(dbUrl, "", "");
+            System.out.println("Connected Successfully");
+        } catch (Exception var1) {
+            System.out.println("" + var1);
+        }
+
+        return con;
+    }
+
+    static {
+        dbUrl = "jdbc:ucanaccess://" + dbFile.trim() + ";memory=true";
+    }
+}
+
